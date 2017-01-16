0e4afe917e79f1858472b6460969ead94055de6d
Osvaldo Lamas <osvaldolamas@gmail.com>
2017-01-15 22:48:21 -0300

    Adding sorting and pagination

--- src/main/resources/static/js/app/controllers/outletController.js
+++ src/main/resources/static/js/app/controllers/outletController.js
@@ -43,15 +43,58 @@ outletControllers.controller('OutletSearchCtrl', ['$scope', 'Outlet','OutletSear
 			$scope.searchOutlet = $scope.selectedOutlet.name;
 			Outlet.get({id: $scope.searchCode}, function(data){
 				$scope.loading = false;
-				$scope.contacts = data;
+				$scope.contacts = data;				
+				$scope.currentPage = 0;
+				$scope.itemsPerPage = 3;
+				$scope.gap = 3;
+				$scope.groupToPages();
 				if(null == data){
-					$scope.errorMsg = "There are no results for name " + $scope.searchCode;
+					$scope.errorMsg = "There are no results for outlet id: " + $scope.searchCode;
 				}
 			}, fail);
 	    };
+     	
+	    $scope.groupToPages = function () {
+	        $scope.pagedContacts = [];	        
+	        for (var i = 0; i < $scope.contacts.length; i++) {
+	            if (i % $scope.itemsPerPage === 0) {
+	                $scope.pagedContacts[Math.floor(i / $scope.itemsPerPage)] = [ $scope.contacts[i] ];
+	            } else {
+	                $scope.pagedContacts[Math.floor(i / $scope.itemsPerPage)].push($scope.contacts[i]);
+	            }
+	        }
+	    };
+	    	    
+	    $scope.range = function (size,start, end) {
+	        var ret = [];
+	           if (size < end) {
+	            end = size;
+	            if(size<$scope.gap){
+	                 start = 0;
+	            }else{
+	                 start = size-$scope.gap;
+	            }
+	        }
+	        for (var i = start; i < end; i++) {
+	            ret.push(i);
+	        }
+	        return ret;
+	    };
 	    
-	    if($stateParams.outletId){
-			$scope.search($stateParams.outletId);
-		}
+	    $scope.prevPage = function () {
+	        if ($scope.currentPage > 0) {
+	            $scope.currentPage--;
+	        }
+	    };
+	    
+	    $scope.nextPage = function () {
+	        if ($scope.currentPage < $scope.pagedContacts.length - 1) {
+	            $scope.currentPage++;
+	        }
+	    };
+	    
+	    $scope.setPage = function () {
+	        $scope.currentPage = this.n;
+	    };
 	}
 	]);
